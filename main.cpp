5c3e8831e66a3c1e9a01a101c3b37cee30ce5a81
Nareshkumar124 <nkk124421@gmail.com>
2023-08-31 08:26:49 +0530

    Search function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ public:
     long long phone;
     string name;
     string email;
+    void displayContact() {
+        cout << "Name: " << this->name << endl;
+        cout << "Phone: " << this->phone << endl;
+        cout << "Email: " << this->email << endl
+             << "\n\n";
+    }
 };
 class PhoneBook
 {
@@ -16,50 +22,61 @@ private:
 
 public:
     PhoneBook() {}
-    void addContact(string name, long long phone, string email)
-    {
-        contact *temp = (contact *)malloc(sizeof(contact));
-        temp->name = name;
-        temp->phone = phone;
-        temp->email = email;
-        contact *iter = head;
-        if (head == NULL)
-        {
-            head = temp;
-        }
-        else
-        {
-            while (iter->next != NULL)
-            {
-                iter = iter->next;
-            }
-            iter->next = temp;
-            temp->pre = iter;
-        }
-    }
-
-    void display()
-    {
-        contact *iter = this->head;
-        int i = 1;
-        cout << "Contacts:  \n";
-        while (iter != NULL)
-        {
-            cout << "Contact " << i << endl;
-            cout << "Name: " << iter->name << endl;
-            cout << "Phone: " << iter->phone << endl;
-            cout << "Email: " << iter->email << endl
-                 << "\n\n";
-            iter = iter->next;
-            i++;
-        }
-    }
+    void addContact(string name, long long phone, string email);
+    void display();
+    void search(string name);
 };
 
+void PhoneBook::addContact(string name, long long phone, string email)
+{
+    contact *temp = (contact *)malloc(sizeof(contact));
+    temp->name = name;
+    temp->phone = phone;
+    temp->email = email;
+    contact *iter = head;
+    if (head == NULL)
+    {
+        head = temp;
+    }
+    else
+    {
+        while (iter->next != NULL)
+        {
+            iter = iter->next;
+        }
+        iter->next = temp;
+        temp->pre = iter;
+    }
+}
+void PhoneBook::display()
+{
+    contact *iter = this->head;
+    int i = 1;
+    cout << "Contacts:  \n";
+    while (iter != NULL)
+    {
+        cout << "Contact " << i << endl;
+        cout << "Name: " << iter->name << endl;
+        cout << "Phone: " << iter->phone << endl;
+        cout << "Email: " << iter->email << endl
+             << "\n\n";
+        iter = iter->next;
+        i++;
+    }
+}
+void PhoneBook::search(string name) {
+    contact *iter = this->head;
+    while(true) {
+        if (iter->name == name) {
+            iter->displayContact();
+            break;
+        }
+    }
+}
 int main()
 {
     PhoneBook data = PhoneBook();
     data.addContact("Naresh Kumar", 1234567890, "nkk124421@gmail.com");
     data.display();
-    getchar();
+    data.search("Naresh Kumar");
 }
